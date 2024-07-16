c74c9d11720d432e02aa3355270f04a1e2012581
Mr - A N A S <mr.anas1003@gmail.com>
2024-07-17 02:03:33 +0500

    added components

--- client/covid-master/about.php
+++ client/covid-master/about.php
@@ -1,289 +1,115 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>COVID-19 Awareness</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+<?php include("./components/navbar.php"); ?>
 
-  <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700;900&display=swap" rel="stylesheet">
-
-  <link rel="stylesheet" href="fonts/icomoon/style.css">
-
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/jquery-ui.css">
-  <link rel="stylesheet" href="css/owl.carousel.min.css">
-  <link rel="stylesheet" href="css/owl.theme.default.min.css">
-  <link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-
-  <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-
-  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-  <link rel="stylesheet" href="fonts/flaticon-covid/font/flaticon.css">
-
-  <link rel="stylesheet" href="css/aos.css">
-
-  <link rel="stylesheet" href="css/style.css">
-
-</head>
-<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+<div class="hero-v1">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6 mr-auto text-center text-lg-left">
+        <span class="d-block subheading">About</span>
+        <h1 class="heading mb-3">About COVID-19</h1>
+        <p class="mb-5">COVID-19 is a highly contagious respiratory illness caused by the novel coronavirus SARS-CoV-2. It was first identified in Wuhan, China in late 2019.</p>
+        <p class="mb-4"><a href="#" class="btn btn-primary">Learn More</a></p>
 
 
-  <div id="overlayer"></div>
-  <div class="loader">
-    <div class="spinner-border text-primary" role="status">
-      <span class="sr-only">Loading...</span>
+
+      </div>
+      <div class="col-lg-6">
+        <figure class="illustration">
+          <img src="images/illustration.png" alt="Image" class="img-fluid">
+        </figure>
+      </div>
+
     </div>
   </div>
+</div>
 
 
-  <div class="site-wrap">
+<!-- MAIN -->
 
-    <div class="site-mobile-menu site-navbar-target">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
+
+
+
+<div class="site-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 mb-4 mb-lg-0">
+        <figure class="img-play-vid">
+          <img src="images/hero_1.jpg" alt="Image" class="img-fluid">
+          <div class="absolute-block d-flex">
+            <span class="text">Watch the Video</span>
+            <a href="https://www.youtube.com/watch?v=9pVy8sRC440" data-fancybox class="btn-play">
+              <span class="icon-play"></span>
+            </a>
+          </div>
+        </figure>
       </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
-
-    
-    <header class="site-navbar light js-sticky-header site-navbar-target" role="banner">
-
-      <div class="container">
-        <div class="row align-items-center">
-
-          <div class="col-6 col-xl-2">
-            <div class="mb-0 site-logo"><a href="index.html" class="mb-0">COVID-19<span class="text-primary">.</span> </a></div>
-          </div>
-
-          <div class="col-12 col-md-10 d-none d-xl-block">
-            <nav class="site-navigation position-relative text-right" role="navigation">
-
-              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li><a href="index.html" class="nav-link">Home</a></li>
-                <li class="has-children">
-                  <a href="prevention.html" class="nav-link">Prevention</a>
-                  <ul class="dropdown">
-                    <li><a href="#" class="nav-link">Stay at home</a></li>
-                    <li><a href="#" class="nav-link">Keep social distancing</a></li>
-                    <li><a href="#" class="nav-link">Wear face masks</a></li>
-                    <li><a href="#" class="nav-link">Wash your hands</a></li>
-                    <li class="has-children">
-                      <a href="#">More Links</a>
-                      <ul class="dropdown">
-                        <li><a href="#">Government Guidelines</a></li>
-                        <li><a href="#">Healthcare Recommendations</a></li>
-                        <li><a href="#">Community Support</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="symptoms.html" class="nav-link">Symptoms</a></li>
-                <li class="active"><a href="about.html" class="nav-link">About</a></li>
-                <li><a href="blog.html" class="nav-link">Blog</a></li>
-                <li><a href="contact.html" class="nav-link">Contact</a></li>
-              </ul>
-            </nav>
-          </div>
-
-
-          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3 text-black"></span></a></div>
-
-        </div>
-      </div>
-
-    </header>
-
-    
-
-
-    <div class="hero-v1">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-6 mr-auto text-center text-lg-left">
-            <span class="d-block subheading">About</span>
-            <h1 class="heading mb-3">About COVID-19</h1>
-            <p class="mb-5">COVID-19 is a highly contagious respiratory illness caused by the novel coronavirus SARS-CoV-2. It was first identified in Wuhan, China in late 2019.</p>
-            <p class="mb-4"><a href="#" class="btn btn-primary">Learn More</a></p>
-
-
-
-          </div>
-          <div class="col-lg-6">
-            <figure class="illustration">
-              <img src="images/illustration.png" alt="Image" class="img-fluid">
-            </figure>
-          </div>
-          
-        </div>
+      <div class="col-lg-5 ml-auto">
+        <h2 class="mb-4 section-heading">What is Coronavirus?</h2>
+        <p>Coronaviruses are a large family of viruses that can cause illness in animals and humans. In humans, they can range from the common cold to more severe diseases like COVID-19.</p>
+        <ul class="list-check list-unstyled mb-5">
+          <li>COVID-19 symptoms include fever, cough, and difficulty breathing.</li>
+          <li>It spreads primarily through respiratory droplets when an infected person coughs, sneezes, or talks.</li>
+          <li>Preventive measures include wearing masks, practicing social distancing, and frequent handwashing.</li>
+        </ul>
+        <p><a href="#" class="btn btn-primary">Learn more</a></p>
       </div>
     </div>
-
-
-    <!-- MAIN -->
-
-
-
-
-    <div class="site-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 mb-4 mb-lg-0">
-            <figure class="img-play-vid">
-              <img src="images/hero_1.jpg" alt="Image" class="img-fluid">
-              <div class="absolute-block d-flex">
-                <span class="text">Watch the Video</span>
-                <a href="https://www.youtube.com/watch?v=9pVy8sRC440" data-fancybox class="btn-play">
-                  <span class="icon-play"></span>
-                </a>
-              </div>
-            </figure>
-          </div>
-          <div class="col-lg-5 ml-auto">
-            <h2 class="mb-4 section-heading">What is Coronavirus?</h2>
-            <p>Coronaviruses are a large family of viruses that can cause illness in animals and humans. In humans, they can range from the common cold to more severe diseases like COVID-19.</p>
-            <ul class="list-check list-unstyled mb-5">
-              <li>COVID-19 symptoms include fever, cough, and difficulty breathing.</li>
-              <li>It spreads primarily through respiratory droplets when an infected person coughs, sneezes, or talks.</li>
-              <li>Preventive measures include wearing masks, practicing social distancing, and frequent handwashing.</li>
-            </ul>
-            <p><a href="#" class="btn btn-primary">Learn more</a></p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="site-section">
-      <div class="container">
-        <div class="row mb-3">
-          <div class="col-lg-7 text-center mx-auto">
-            <h2 class="section-heading">Team</h2>
-            <p>Meet the dedicated team behind our efforts to provide accurate information and support during the COVID-19 pandemic.</p>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="team">
-              <img src="images/person_1.jpg" alt="Image" class="img-fluid">
-              <h3>Dr. Sarah Johnson</h3>
-              <p>Dr. Sarah Johnson is an infectious disease specialist with years of experience in managing viral outbreaks.</p>
-              <p>
-                <span class="d-block mb-2"><strong>Connect on social</strong></span>
-                <a href="#" class="p-2 icon-facebook"></a>
-                <a href="#" class="p-2 icon-twitter"></a>
-                <a href="#" class="p-2 icon-linkedin"></a>
-                <a href="#" class="p-2 icon-instagram"></a>
-              </p>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="team">
-              <img src="images/person_2.jpg" alt="Image" class="img-fluid">
-              <h3>Dr. Mark Davis</h3>
-              <p>Dr. Mark Davis is a public health expert specializing in epidemiology and disease prevention strategies.</p>
-              <p>
-                <span class="d-block mb-2"><strong>Connect on social</strong></span>
-                <a href="#" class="p-2 icon-facebook"></a>
-                <a href="#" class="p-2 icon-twitter"></a>
-                <a href="#" class="p-2 icon-linkedin"></a>
-                <a href="#" class="p-2 icon-instagram"></a>
-              </p>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="team">
-              <img src="images/person_3.jpg" alt="Image" class="img-fluid">
-              <h3>Dr. Emily Clark</h3>
-              <p>Dr. Emily Clark is a virologist specializing in coronavirus research and vaccine development.</p>
-              <p>
-                <span class="d-block mb-2"><strong>Connect on social</strong></span>
-                <a href="#" class="p-2 icon-facebook"></a>
-                <a href="#" class="p-2 icon-twitter"></a>
-                <a href="#" class="p-2 icon-linkedin"></a>
-                <a href="#" class="p-2 icon-instagram"></a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="row">
-              <div class="col-6 col-md-3 mb-4 mb-md-0">
-                <h3 class="footer-heading mb-4">Navigate</h3>
-                <ul class="list-unstyled">
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">Prevention</a></li>
-                  <li><a href="#">Symptoms</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Blog</a></li>
-                  <li><a href="#">Contact</a></li>
-                </ul>
-              </div>
-              <div class="col-6 col-md-3 mb-4 mb-md-0">
-                <h3 class="footer-heading mb-4">Prevention</h3>
-                <ul class="list-unstyled">
-                  <li><a href="#">Stay at home</a></li>
-                  <li><a href="#">Keep social distancing</a></li>
-                  <li><a href="#">Wear face masks</a></li>
-                  <li><a href="#">Wash your hands</a></li>
-                </ul>
-              </div>
-              <div class="col-6 col-md-3 mb-4 mb-md-0">
-                <h3 class="footer-heading mb-4">Resources</h3>
-                <ul class="list-unstyled">
-                  <li><a href="#">WHO Guidelines</a></li>
-                  <li><a href="#">CDC Updates</a></li>
-                  <li><a href="#">Local Health Authorities</a></li>
-                </ul>
-              </div>
-              <div class="col-6 col-md-3 mb-4 mb-md-0">
-                <h3 class="footer-heading mb-4">Connect</h3>
-                <ul class="list-unstyled">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Careers</a></li>
-                  <li><a href="#">Advertise</a></li>
-                  <li><a href="#">Media</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row mt-5 text-center">
-          <div class="col-12">
-            <p>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-          </div>
-        </div>
-      </div>
-    </footer>
-
   </div>
+</div>
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/jquery.easing.1.3.js"></script>
-  <script src="js/aos.js"></script>
-  <script src="js/jquery.fancybox.min.js"></script>
-  <script src="js/jquery.sticky.js"></script>
-  <script src="js/isotope.pkgd.min.js"></script>
+<div class="site-section">
+  <div class="container">
+    <div class="row mb-3">
+      <div class="col-lg-7 text-center mx-auto">
+        <h2 class="section-heading">Team</h2>
+        <p>Meet the dedicated team behind our efforts to provide accurate information and support during the COVID-19 pandemic.</p>
+      </div>
+    </div>
 
-  <script src="js/main.js"></script>
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="team">
+          <img src="images/person_1.jpg" alt="Image" class="img-fluid">
+          <h3>Dr. Sarah Johnson</h3>
+          <p>Dr. Sarah Johnson is an infectious disease specialist with years of experience in managing viral outbreaks.</p>
+          <p>
+            <span class="d-block mb-2"><strong>Connect on social</strong></span>
+            <a href="#" class="p-2 icon-facebook"></a>
+            <a href="#" class="p-2 icon-twitter"></a>
+            <a href="#" class="p-2 icon-linkedin"></a>
+            <a href="#" class="p-2 icon-instagram"></a>
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="team">
+          <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+          <h3>Dr. Mark Davis</h3>
+          <p>Dr. Mark Davis is a public health expert specializing in epidemiology and disease prevention strategies.</p>
+          <p>
+            <span class="d-block mb-2"><strong>Connect on social</strong></span>
+            <a href="#" class="p-2 icon-facebook"></a>
+            <a href="#" class="p-2 icon-twitter"></a>
+            <a href="#" class="p-2 icon-linkedin"></a>
+            <a href="#" class="p-2 icon-instagram"></a>
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="team">
+          <img src="images/person_3.jpg" alt="Image" class="img-fluid">
+          <h3>Dr. Emily Clark</h3>
+          <p>Dr. Emily Clark is a virologist specializing in coronavirus research and vaccine development.</p>
+          <p>
+            <span class="d-block mb-2"><strong>Connect on social</strong></span>
+            <a href="#" class="p-2 icon-facebook"></a>
+            <a href="#" class="p-2 icon-twitter"></a>
+            <a href="#" class="p-2 icon-linkedin"></a>
+            <a href="#" class="p-2 icon-instagram"></a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-</body>
-</html>
+<?php include("./components/footer.php"); ?>
