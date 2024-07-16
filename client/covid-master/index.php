c333f1dcdbe0b70d7840f3c7d39975625e2939a9
TechNovas <agha.abdulqadirbusiness@gmail.com>
2024-07-17 01:45:09 +0500

    abdul qadir

--- client/covid-master/index.php
+++ client/covid-master/index.php
@@ -57,37 +57,20 @@
         <div class="row align-items-center">
 
           <div class="col-6 col-xl-2">
-            <div class="mb-0 site-logo"><a href="index.html" class="mb-0">Covid<span class="text-primary">.</span> </a></div>
+            <div class="mb-0 site-logo"><a href="about.php" class="mb-0">Covid<span class="text-primary">.</span> </a></div>
           </div>
 
           <div class="col-12 col-md-10 d-none d-xl-block">
             <nav class="site-navigation position-relative text-right" role="navigation">
 
               <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li class="active"><a href="index.html" class="nav-link">Home</a></li>
-                <li class="has-children">
-                  <a href="prevention.html" class="nav-link">Prevention</a>
-                  <ul class="dropdown">
-                    <li><a href="#" class="nav-link">Stay at home</a></li>
-                    <li><a href="#" class="nav-link">Keep social distancing</a></li>
-                    <li><a href="#" class="nav-link">Wear facemasl</a></li>
-                    <li><a href="#" class="nav-link">Wash your hands</a></li>
-                    <li class="has-children">
-                      <a href="#">More Links</a>
-                      <ul class="dropdown">
-                        <li><a href="#">Menu One</a></li>
-                        <li><a href="#">Menu Two</a></li>
-                        <li><a href="#">Menu Three</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="symptoms.html" class="nav-link">Symptoms</a></li>
-                <li><a href="about.html" class="nav-link">About</a></li>
+                <li class="active"><a href="index.php" class="nav-link">Home</a></li>
+                  
+                <li><a href="./about.php" class="nav-link">About</a></li>
 
 
-                <li><a href="blog.html" class="nav-link">Blog</a></li>
-                <li><a href="contact.html" class="nav-link">Contact</a></li>
+                <li><a href="blog.php" class="nav-link">Blog</a></li>
+                <li><a href="contact.php" class="nav-link">Contact</a></li>
               </ul>
             </nav>
           </div>
