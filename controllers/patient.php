a93a0628d68de261395df68ee63bb74d37186176
Mr - A N A S <mr.anas1003@gmail.com>
2024-07-24 02:36:11 +0500

    backend of login is complated

--- controllers/patient.php
+++ controllers/patient.php
@@ -1,32 +1,57 @@
 <?php
-require_once "./config/Database.php";
-require_once "./models/User.php";
+require_once __DIR__ . '/../config/Database.php';
+require_once __DIR__ . '/../models/User.php';
+
 class UserController
 {
+    private $db;
+    private $conn;
+    function __construct()
+    {
+        $this->db = new Database;
+        $this->conn = $this->db->getConnection();
+    }
 
     public function login()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $username = trim($_POST['username']);
+            $email = trim($_POST['email']);
             $password = trim($_POST['password']);
 
-            $conn = new Database;
-            $userModel = new UserModel($conn);
-            $user = $userModel->login($username, $password);
+            if (!empty($email) && !empty($password)) {
+                $userModel = new UserModel($this->conn);
+                $user = $userModel->login($email, $password);
 
-            if ($user) {
-                // User authenticated successfully
-                session_start();
-                $_SESSION['user_id'] = $user['id'];
-                $_SESSION['username'] = $user[$username];
-                header('Location: /dashboard');
-            } else {
-                // Authentication failed
-                header('Location: /login?error=Invalid+credentials');
+                if ($user) {
+                    // User authenticated successfully
+                    session_start();
+                    $_SESSION['user_id'] = $user['id'];
+                    $_SESSION['email'] = $user[$email];
+
+                    // need to correct this path <<<<<<<<<<<<<<<<<<
+                    header('Location: ../views/patient/welcome.php');
+                    exit;
+                } else {
+
+                    // Authentication failed
+                    // need to correct this path <<<<<<<<<<<<<<<<<<
+                    header('Location: ../views/patient/login.php?error=Invalid+credentials');
+                    $this->db->closeConnection();
+                    exit;
+                }
             }
         } else {
+
             // Show the login form
-            require 'views/login.php';
+            header('Location: /login.php');
+            $this->db->closeConnection();
+            exit;
         }
     }
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $UserCon = new UserController;
+    $UserCon->login();
 }
--- models/User.php
+++ models/User.php
@@ -7,7 +7,16 @@ class UserModel{
         $this->conn = $conn;
     }
 
-    function login(){
-
+    function login($email, $password){
+        $query = "SELECT * FROM `user` WHERE email='$email' AND password='$password'";
+        $result= mysqli_query($this->conn, $query);
+        if ($result) {
+            $num = mysqli_num_rows($result);
+            if ($num > 0) {
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 }
