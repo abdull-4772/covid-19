0058d5e7af31e1f4f19a8316ef987fa609afe489
Mr - A N A S <mr.anas1003@gmail.com>
2024-07-23 21:42:39 +0500

    public file setup

--- public/partials/header.php
+++ public/partials/header.php
@@ -10,22 +10,22 @@
 
   <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/jquery-ui.css">
-  <link rel="stylesheet" href="css/owl.carousel.min.css">
-  <link rel="stylesheet" href="css/owl.theme.default.min.css">
-  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./public/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./public/css/jquery-ui.css">
+  <link rel="stylesheet" href="./public/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./public/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./public/css/owl.theme.default.min.css">
 
-  <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="./public/css/jquery.fancybox.min.css">
 
-  <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="./public/css/bootstrap-datepicker.css">
 
-  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-  <link rel="stylesheet" href="fonts/flaticon-covid/font/flaticon.css">
+  <link rel="stylesheet" href="./public/fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="./public/fonts/flaticon-covid/font/flaticon.css">
 
-  <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="./public/css/aos.css">
 
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="./public/css/style.css">
 
 </head>
 
