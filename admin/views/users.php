be3656675c6fad28e1b1a63b3ae53e952be3c118
Mr - A N A S <mr.anas1003@gmail.com>
2024-08-07 00:20:51 +0500

    modified in alert

--- admin/views/users.php
+++ admin/views/users.php
@@ -13,22 +13,35 @@
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        if ($_GET['status'] == "deleted") {
-            echo "<script>
-            document.addEventListener('DOMContentLoaded', function() {
-                showDeletedAlert();
-            });
-          </script>";
+        if (isset($_GET['status'])) {
+            if ($_GET['status'] == "deleted") {
+                echo "<script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    showDeletedAlert();
+                });
+            </script>";
+            } elseif ($_GET['status'] == "edited") {
+                echo "<script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    showUpdatedAlert();
+                });
+            </script>";
+            }
         }
     }
     ?>
-    <!-- Deleted Alert box -->
+    <!-- Alerts -->
     <div id="customAlertOverlay" class="custom-overlay">
-        <div class="custom-alert-box">
+        <div id="deletedAlertBox" class="custom-alert-box">
             <h2>User Deleted</h2>
             <p>The user has been successfully deleted.</p>
         </div>
+        <div id="updatedAlertBox" class="custom-alert-box">
+            <h2>User Updated</h2>
+            <p>The user information has been successfully updated.</p>
+        </div>
     </div>
+
 
 
     <div class="main">
@@ -39,7 +52,7 @@
                     <th>id</th>
                     <th>name</th>
                     <th>email</th>
-                    <th>password</th>
+                    <!-- <th>password</th> <td>' . $row['password'] . '</td>-->
                     <th>age</th>
                     <th>gender</th>
                     <th>address</th>
@@ -59,7 +72,6 @@
                         <td>' . $row['id'] . '</td>
                         <td>' . $row['name'] . '</td>
                         <td>' . $row['email'] . '</td>
-                        <td>' . $row['password'] . '</td>
                         <td>' . $row['age'] . '</td>
                         <td>' . $row['gender'] . '</td>
                         <td>' . $row['address'] . '</td>
@@ -83,21 +95,30 @@
     </div>
 
     <script>
-        // showing Alert Box
-        let Back = document.querySelector("#customAlertOverlay");
-        let dltAlertBox = document.querySelector(".custom-alert-box");
+        // Showing Alert Boxes
+        let alertBackground = document.querySelector("#customAlertOverlay");
+        let deletedAlertBox = document.querySelector("#deletedAlertBox");
+        let updatedAlertBox = document.querySelector("#updatedAlertBox");
 
         function showDeletedAlert() {
-            Back.style.display = "block";
-            dltAlertBox.style.top = "40%";
+            alertBackground.style.display = "block";
+            deletedAlertBox.style.top = "40%";
             setTimeout(() => {
-                removeDeletedAlrt()
-            }, 2200)
+                removeAlert(deletedAlertBox);
+            }, 2100);
         }
 
-        function removeDeletedAlrt() {
-            Back.style.display = "none";
-            dltAlertBox.style.top = "-120%";
+        function showUpdatedAlert() {
+            alertBackground.style.display = "block";
+            updatedAlertBox.style.top = "40%";
+            setTimeout(() => {
+                removeAlert(updatedAlertBox);
+            }, 2100);
+        }
+
+        function removeAlert(alertBox) {
+            alertBox.style.top = "-120%";
+            alertBackground.style.display = "none";
         }
     </script>
 </body>
